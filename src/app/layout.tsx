f3a4e5030f3743b26468e10ff94218935fc758a0
MAGNUNMAX <torre784@gmail.com>
2026-01-13 23:57:21 -0500

    updating layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: "ProfolioCraft",
+  title: "Pedro's Portfolio",
   description: "Creative Developer & UI/UX Designer Portfolio",
 };
 
